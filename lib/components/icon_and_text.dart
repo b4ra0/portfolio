980bf1f471b57cefe4431cb6a5eccf1b54c9af10
Lucas Barão <lucas.baraom@gmail.com>
2025-05-10 00:50:32 -0300

    ajusting padding at the icon and text widget

--- lib/components/icon_and_text.dart
+++ lib/components/icon_and_text.dart
@@ -8,20 +8,23 @@ class IconAndText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(
-          icon,
-        ),
-        const SizedBox(width: 8),
-        Text(
-          text,
-          style: const TextStyle(
-            fontSize: 16,
-            color: Colors.white,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 3.0),
+      child: Row(
+        children: [
+          Icon(
+            icon,
           ),
-        ),
-      ],
+          const SizedBox(width: 8),
+          Text(
+            text,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
